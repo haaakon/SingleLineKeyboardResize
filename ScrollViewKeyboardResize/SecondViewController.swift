f4def91de64f7bf0b3804a245c6dc1a8081d81ca
Håkon Bogen <hakon.bogen@shortcut.no>
2015-02-12 08:53:42 +0100

    Initial Commit

---
+++ ScrollViewKeyboardResize/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ScrollViewKeyboardResize
+//
+//  Created by Håkon Bogen on 12/02/15.
+//  Copyright (c) 2015 haaakon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
