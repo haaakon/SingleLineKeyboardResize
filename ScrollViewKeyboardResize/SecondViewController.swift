8cb3ef2cdf99c048317db43091e84d4d8f2f986c
Håkon Bogen <hakon.bogen@shortcut.no>
2015-02-21 09:59:37 +0100

    add example with an ordinary scrollview

--- ScrollViewKeyboardResize/SecondViewController.swift
+++ ScrollViewKeyboardResize/SecondViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var scrollView : UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupKeyboardNotifcationListenerForScrollView(scrollView)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        scrollView.contentSize = CGSizeMake(scrollView.frame.size.width, 700)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
